f2104a9930eab788e4fbc7cf2182df1e41fc8658
Amsav23 <130859471+Amsav23@users.noreply.github.com>
2023-11-28 16:49:50 -0500

    Episodes.jsx functions properly

--- src/pages/Episodes/Episodes.jsx
+++ src/pages/Episodes/Episodes.jsx
@@ -1,10 +1,20 @@
 import React, { useEffect } from 'react'
 import './Episodes.css'
 import axios from 'axios'
+import CharacterCard from '../../components/CharacterCard/CharacterCard'
 
 function Episodes() {
   //create state for the array of options
   const [options, setOptions] = React.useState([])
+
+  //state for option selected
+  const [selectedOption, setSelectedOption] = React.useState(1)
+
+  //state for episode info
+  const [selectedEpisode, setSelectedEpisode] = React.useState('')
+
+  //state for characters
+  const [characterList, setCharacterList] = React.useState([])
 
   //when I select an episode, the page shows info and characters
   //from that episode
@@ -29,8 +39,48 @@ function Episodes() {
     }, [] //runs once when the page loads
   )
 
-  const handleSelectChange = () => {
-    console.log('episode selected')
+  //this useEffect runs anytime selectedOption changes
+  useEffect (
+    () => {
+      console.log('selected episode', selectedOption)
+      //make API calls
+      fetchEpisodeData()
+
+    }, [selectedOption]
+  )
+
+  //https://rickandmortyapi.com/api/episode/2
+  const fetchEpisodeData = async () => {
+    console.log('make API call')
+    try {
+      //make API call to get data episode data
+        const res = await axios.get(`https://rickandmortyapi.com/api/episode/${selectedOption}`)
+        console.log(res.data)
+        //store in state
+        setSelectedEpisode(res.data)
+
+        console.log(res.data.characters)
+        //we need to make all these API calls to get character data
+        const episodeCharacters = await Promise.all (
+          res.data.characters.map(url => {
+            return axios.get(url).then(res => res.data)
+          })
+        )
+        console.log(episodeCharacters)
+        //store in state
+        setCharacterList(episodeCharacters)
+
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+
+  const handleSelectChange = (e) => {
+    //what number was selected?
+    console.log('episode selected', e.target.value)
+    //store in state
+    setSelectedOption(e.target.value)
   }
 
   return (
@@ -39,10 +89,25 @@ function Episodes() {
         <label htmlFor='select-episode'>Select an episode: </label>
         <select id='select-episode' onChange={handleSelectChange}>
           {
-            options.map(num => <option>{`Episode ${num}`}</option>)
+            options.map(num => <option key={num} value={num}>{`Episode ${num}`}</option>)
           }
         </select>
       </div>
+
+      <div>
+        <div className='episode-data'>
+          <p>Episode Name: {selectedEpisode?.name}</p>
+          <p>Air Date: {selectedEpisode?.air_date}</p>
+        </div>
+        <div className='character-container'>
+          {
+            characterList.map(item => <CharacterCard
+              key={item.id} character={item}/>)
+          }
+        </div>
+
+      </div>
+
     </div>
   )
 }
